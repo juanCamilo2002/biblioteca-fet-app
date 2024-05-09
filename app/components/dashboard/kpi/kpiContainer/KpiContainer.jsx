1f48084365c5591c15af07c750e6ee1a8207b630
juanCamilo2002 <juan2002djfh@gmail.com>
2024-05-09 15:42:00 -0500

    Cambios en las peticiones de los stats del dashboard

--- app/components/dashboard/kpi/kpiContainer/KpiContainer.jsx
+++ app/components/dashboard/kpi/kpiContainer/KpiContainer.jsx
@@ -1,59 +1,74 @@
+"use client";
 import { getBookMostReserv, getCountBooksRegister, getReservasCountMonth } from "@api";
 import Kpi from "../Kpi";
 import styles from './kpicontainer.module.css';
 import { FiBook } from 'react-icons/fi'
 import { GiBookshelf } from "react-icons/gi";
 import { HiOutlineSaveAs } from 'react-icons/hi'
+import { use, useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
-const KpiContainer = async () => {
-    try {
-        const reservationsMonthly = await getReservasCountMonth();
-        const totalBooks = await getCountBooksRegister();
-        const bookMostReserv = await getBookMostReserv();
+const KpiContainer = () => {
+    const [bookMostReserv, setBookMostReserv] = useState({});
+    const [totalBooks, setTotalBooks] = useState({});
+    const [reservationsMonthly, setReservationsMonthly] = useState({});
+    useEffect(() => {
+        const getStats = async () => {
+            try {
+                const bookMostReservResponse = await getBookMostReserv();
+                const totalBooksResponse = await getCountBooksRegister();
+                const reservationsMonthlyResponse = await getReservasCountMonth();
+                setBookMostReserv(bookMostReservResponse);
+                setTotalBooks(totalBooksResponse);
+                setReservationsMonthly(reservationsMonthlyResponse);
 
-        const stats = [
-            {
-                stat: reservationsMonthly.cantidadReservas,
-                name: "Reservas",
-                label: "Reservas en el mes",
-                icon: <HiOutlineSaveAs size={25} />,
-                placeholder: "No hay reservas en el mes"
-            },
-            {
-                stat: totalBooks.cantidadLibros,
-                name: "Libros",
-                label: "Libros registrados",
-                icon: <GiBookshelf size={25} />,
-                placeholder: "No hay libros registrados"
-            },
-            {
-                stat: bookMostReserv.libroMasReservado,
-                name: "Libro",
-                label: "Libro más reservado",
-                icon: <FiBook size={25} />,
-                placeholder: "No hay libros reservados"
+            } catch (error) {
+                toast.error("Error al obtener las estadisticas")
             }
-        ];
+        }
+        getStats();
+    }, [])
 
-        return (
-            <div className={styles.container}>
-                {stats.map((stat, index) => (
-                    <Kpi
-                        key={index}
-                        stat={stat.stat}
-                        name={stat.name}
-                        label={stat.label}
-                        icon={stat.icon}
-                        placeholder={stat.placeholder}
-                        maxLength={25}
-                    />
-                ))}
-            </div>
-        );
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        return <div>Error al obtener datos de la API</div>;
-    }
+    const stats = [
+        {
+            stat: reservationsMonthly.cantidadReservas,
+            name: "Reservas",
+            label: "Reservas en el mes",
+            icon: <HiOutlineSaveAs size={25} />,
+            placeholder: "No hay reservas en el mes"
+        },
+        {
+            stat: totalBooks.cantidadLibros,
+            name: "Libros",
+            label: "Libros registrados",
+            icon: <GiBookshelf size={25} />,
+            placeholder: "No hay libros registrados"
+        },
+        {
+            stat: bookMostReserv.libroMasReservado,
+            name: "Libro",
+            label: "Libro más reservado",
+            icon: <FiBook size={25} />,
+            placeholder: "No hay libros reservados"
+        }
+    ];
+
+    return (
+        <div className={styles.container}>
+            {stats.map((stat, index) => (
+                <Kpi
+                    key={index}
+                    stat={stat.stat}
+                    name={stat.name}
+                    label={stat.label}
+                    icon={stat.icon}
+                    placeholder={stat.placeholder}
+                    maxLength={25}
+                />
+            ))}
+        </div>
+    );
+
 }
 
 export default KpiContainer;

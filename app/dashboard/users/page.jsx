c5a437bda77257b7dc5bf83107c51a960e4d0284
salseo98 <salseo98@outlook.com>
2024-05-09 08:47:15 -0500

    View users

---
+++ app/dashboard/users/page.jsx
@@ -0,0 +1,11 @@
+import UserTable from "@/app/components/dashboard/users/userstable/UserTable"
+
+function page() {
+  return (
+    <div>
+        <UserTable/>
+    </div>
+  )
+}
+
+export default page

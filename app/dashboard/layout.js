cf30b6e9dee230875e2e3e79fc6f451ef6098f2d
juanCamilo2002 <juan2002djfh@gmail.com>
2024-05-14 14:30:36 -0500

    update module books

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,15 +1,19 @@
+"use client";
 import styles from "./layout.module.css";
 import SideBar from "../components/dashboard/sideBar/SideBar";
 import { CiSearch } from "react-icons/ci";
 import PrivateAdminRoute from "../components/PrivateAdminRoute";
+import { useSidebar } from "@/context/SidebarProvider";
+import { useState } from "react";
 
 
 const Layout = ({ children }) => {
+  const { isOpen } = useSidebar();
   return (
     <PrivateAdminRoute >
       <div className={styles.container}>
       <SideBar />
-      <div className={styles.content}>
+      <div className={`${styles.content} ${isOpen && styles.active}`}>
         <div className={styles.containerSearch}>
           <div className={styles.inputSearch}>
             <input type="text" placeholder='Buscar...' className={styles.input}/>

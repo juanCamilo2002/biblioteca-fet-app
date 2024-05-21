bc95abe68bc76e83864b002cfd178ab529a912d8
salseo98 <salseo98@outlook.com>
2024-05-20 19:16:52 -0500

    Vista de Reportes Admin

---
+++ app/components/dashboard/reports/reportdatatable/ReportTable.jsx
@@ -0,0 +1,102 @@
+"use client"
+import styles from './report.module.css'
+import DataTable from 'react-data-table-component'
+
+function ReportTable() {
+
+  const columns =[
+    {
+      name: "Nombre",
+      selector: (row) => row.nombre,
+      sortable: true
+    },
+    {
+      name: "Semestre",
+      selector: (row) => row.semestre,
+      sortable: true
+    },
+    {
+      name: "Ciclo",
+      selector: (row) => row.ciclo,
+      sortable: true
+    },
+    {
+      name: "Programa",
+      selector: (row) => row.programa,
+      sortable: true
+    },
+    {
+      name: "Genero",
+      selector: (row) => row.genero,
+      sortable: true
+    },
+    {
+      name: "Cantidad de libros",
+      selector: (row) => row.libros,
+      sortable: true
+    }
+  ]
+
+  const datos = [
+    {
+    nombre: 'Joan Galindo',
+    semestre: 'Noveno',
+    ciclo: 'Profesional',
+    programa: 'Software',
+    genero: 'Masculino',
+    libros: 3
+    },
+    
+  ]
+
+  const paginationComponentOptions = {
+    rowsPerPageText: 'Filas por página',
+    rangeSeparatorText: 'de',
+    selectAllRowsItem: true,
+    selectAllRowsItemText: 'Todos',
+  };
+
+  return (
+    <div>
+      <h1 className={styles.titleReport}>Reportes</h1>
+      <div className={styles.container}>
+        <div>
+          <select className={styles.btnOption}>
+            <option >Fecha de carga</option>
+            <option >Enero</option>
+            <option >Febrero</option>
+            <option >Marzo</option>
+            <option >Abril</option>
+            <option >Mayo</option>
+            <option >Junio</option>
+            <option >Julio</option>
+            <option >Agosto</option>
+            <option >Septiembre</option>
+            <option >Octubre</option>
+            <option >Noviembre</option>
+            <option >Diciembre</option>
+          </select> 
+          <select  className={styles.btnOption}>
+            <option >Tipo</option>
+          </select> 
+        
+        </div>
+        
+       
+      <DataTable
+      columns={columns}
+      data={datos}
+      paginationPerPage={5}
+      pagination
+      paginationComponentOptions={paginationComponentOptions}
+      />
+      <div className={styles.btnReport}>
+        <button>Generar Reporte</button>
+      </div>
+    </div>
+    </div>
+    
+  )
+}
+
+export default ReportTable
---
+++ app/dashboard/reports/page.jsx
@@ -0,0 +1,11 @@
+import ReportTable from "@/app/components/dashboard/reports/reportdatatable/ReportTable";
+
+const page = () => {
+  return (
+    <div>
+        <ReportTable/>
+    </div>
+  )
+};
+
+export default page;
